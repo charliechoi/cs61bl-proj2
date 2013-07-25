5902f576422ce34bf68db7fa2c27e8e0c34bc6e7
charliechoi <charlie.choi@berkeley.edu>
2013-07-25 14:19:28 -0700

    Create LineNumber.java

---
+++ LineNumber.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class LineNumber {
+  
+	private String LineNum;
+	
+	public  LineNumber(){
+		
+		LineNum = "1";
+	}
+	public String NewLine (){
+
+		int temp = Integer.parseInt(LineNum.substring(LineNum.length()-1));
+		temp++;
+		LineNum = LineNum.substring(0,LineNum.length()-1) + Integer.toString(temp);
+		
+		return LineNum;
+	}
+	public String NewSubLine(){
+		LineNum = LineNum +".1";
+		
+		return LineNum;
+	}
+	public String DeleteSub(){
+		LineNum = LineNum.substring(0,LineNum.length()-2);
+		
+		return LineNum;
+	}
+	// sets LineNum to correct next Line Number
+	
+	
+	public String get(){
+		return LineNum;
+	}
+	
+	
+public static void main (String []args){
+	LineNumber blah = new LineNumber();
+
+	
+	
+	}
+}
+
