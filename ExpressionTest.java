215202db058821620da1ed4b058726431275bf76
PeterAguiar <paguiar@oberlin.edu>
2013-07-27 16:34:37 -0700

    Update ExpressionTest.java

--- ExpressionTest.java
+++ ExpressionTest.java
@@ -3,30 +3,26 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 
-public class ExpressionTest {
+public class ExpressionTest{
 
 	@Test
 	public void printTest() {
-		
+
 			Expression a = new Expression("(a&b)");
 			Expression b = new Expression("(c&d)");
 			a.expr();
 			a.print();
 			b.expr();
 			b.print();
-	}
-	public void aStringTest(){
-		Expression a = new Expression("(a&b)");
-		Expression b = new Expression("(c&d)");
+
 		assertTrue(a.aString().equals("(a&b)"));
 		assertFalse(a.aString().equals("a"));
 		assertTrue(b.aString().equals("(c&d)"));
 		assertFalse(a.aString().equals(b.aString()));
 		assertTrue(a.aString().equals(a.aString()));
-	}
-	public void compareTest(){
-			Expression a = new Expression("(a&b)");
-			Expression b = new Expression("(c&d)");
+
+			//Expression a = new Expression("(a&b)");
+			//Expression b = new Expression("(c&d)");
 			assertTrue(a.compare(b));
 			Expression c = new Expression("(c|d)");
 			assertFalse(a.compare(c));
@@ -41,11 +37,10 @@ public class ExpressionTest {
 			Expression h = new Expression("(a|(b=>(a|b)))");
 			Expression k = new Expression("((x|y)|((x&b)=>((x|y)|(x&b))))");
 			assertTrue(h.compare(k));
-			
+			assertFalse(a.compare(k));
+			System.out.println(a.compare(k));
+			System.out.println(h.compare(k));
 			}
-		
+
 	}
-
-
-
 
