c2bc586ca9bdb279e59b60a943ee658992f0973a
tanyapongyulee <pongyulee521@gmail.com>
2013-07-27 23:38:32 -0700

    Update ExpressionTest.java

--- ExpressionTest.java
+++ ExpressionTest.java
@@ -1,28 +1,60 @@
-import static org.junit.Assert.*;
+
+import junit.framework.TestCase;
 
 import org.junit.Test;
 
 
-public class ExpressionTest{
+public class ExpressionTest extends TestCase{
 
 	@Test
-	public void printTest() {
-
-			Expression a = new Expression("(a&b)");
-			Expression b = new Expression("(c&d)");
-			a.expr();
-			a.print();
-			b.expr();
-			b.print();
-
+	
+	public static void test2(){
+		System.out.println("Start Testing");
+	}
+	public static void testisOOK(){
+		boolean result=false;
+		Expression a = new Expression("a&d");
+		try{
+			a.isOOK();		
+		}catch (IllegalLineException e){
+			System.out.println(e.getMessage());
+			result=true;
+		}assertTrue(result);
+	}
+	public static void testisOOK2(){
+		boolean result=false;
+		Expression a = new Expression("a&d))");
+		try{
+			a.isOOK();		
+		}catch (IllegalLineException e){
+			System.out.println(e.getMessage());
+			result=true;
+		}assertTrue(result);
+	}
+	
+	
+	public static void test3(){
+		Expression a = new Expression("(a&b)");
+		Expression b = new Expression("(c&d)");
+		a.expr();
+		a.print();
+		b.expr();
+		b.print();
+		
+	}
+	public void aStringTest(){
+		System.out.println("abbb");
+		Expression a = new Expression("(a&b)");
+		Expression b = new Expression("(c&d)");
 		assertTrue(a.aString().equals("(a&b)"));
 		assertFalse(a.aString().equals("a"));
 		assertTrue(b.aString().equals("(c&d)"));
 		assertFalse(a.aString().equals(b.aString()));
 		assertTrue(a.aString().equals(a.aString()));
-
-			//Expression a = new Expression("(a&b)");
-			//Expression b = new Expression("(c&d)");
+	}
+	public void compareTest(){
+			Expression a = new Expression("(a&b)");
+			Expression b = new Expression("(c&d)");
 			assertTrue(a.compare(b));
 			Expression c = new Expression("(c|d)");
 			assertFalse(a.compare(c));
@@ -37,10 +69,10 @@ public class ExpressionTest{
 			Expression h = new Expression("(a|(b=>(a|b)))");
 			Expression k = new Expression("((x|y)|((x&b)=>((x|y)|(x&b))))");
 			assertTrue(h.compare(k));
-			assertFalse(a.compare(k));
-			System.out.println(a.compare(k));
-			System.out.println(h.compare(k));
+			
 			}
-
+	
+	
+	
 	}
 
